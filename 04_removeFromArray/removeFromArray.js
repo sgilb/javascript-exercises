c597fc788643e2d75e9ba6cbe5032ed6072dbcfa
sgilb <32495429+sgilb@users.noreply.github.com>
2023-10-26 23:09:37 +0100

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+  const args = Array.from(arguments);
+  let array = args[0];
 
+  for (let i = 1; i < args.length; i++) {
+    let index = array.indexOf(arguments[i]);
+
+    if (!array[index]) continue;
+    array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
