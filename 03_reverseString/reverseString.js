8506cf833d88e44628e53ff69d87f2f435d28356
sgilb <32495429+sgilb@users.noreply.github.com>
2023-10-26 22:36:15 +0100

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  textArray = Array.from(text);
+  return textArray.reverse().join("");
 };
 
 // Do not edit below this line
