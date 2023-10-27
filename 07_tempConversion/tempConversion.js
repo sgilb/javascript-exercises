ddb805802b17d2ab03466fe7c801a658a41f3fe4
sgilb <32495429+sgilb@users.noreply.github.com>
2023-10-27 23:44:45 +0100

    Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5 / 9);
+  return Math.round(cTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = (cTemp * (9 / 5) + 32);
+  return Math.round(fTemp * 10) / 10;
 };
 
 // Do not edit below this line
