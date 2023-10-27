aae27f48a561a5f94f43216019d9cf5d43d0e7b3
sgilb <32495429+sgilb@users.noreply.github.com>
2023-10-27 23:27:43 +0100

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  if (
+    first < 0 ||
+    typeof first !== "number" ||
+    last < 0 ||
+    typeof last !== "number"
+  )
+    return "ERROR";
 
+  let firstNum = first > last ? last : first;
+  let lastNum = last < first ? first : last;
+
+  let result = 0;
+  for (let i = firstNum; i <= lastNum; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
