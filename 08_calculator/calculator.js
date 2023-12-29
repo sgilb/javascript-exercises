c64d642e15c9dcc4f7579b8ae14cc691c8d8d8f5
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-29 23:50:36 +0000

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum,item) => sum + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product,item) => product * item)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let product = 1;
+  for (i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
