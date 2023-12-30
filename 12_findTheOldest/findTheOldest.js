49fc67682a42fd632fbebc645329bd0616c9bcac
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-30 01:00:30 +0000

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) =>
+    getAge(oldest.yearOfBirth, oldest.yearOfDeath) <
+    getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+      ? currentPerson
+      : oldest
+  );
+};
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
