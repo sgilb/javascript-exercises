147978923db8ff8c416a1987644d009e93996e88
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-30 00:07:21 +0000

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  lower = string.toLowerCase();
+  alpha = lower.replace(/[^a-z0-9]/g, "");
 
+  return alpha === alpha.split("").reverse().join("");
 };
 
 // Do not edit below this line
